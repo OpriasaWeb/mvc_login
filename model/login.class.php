a6cc8c65c3f2e503c0726db357e0ffc0df30f89e
Jeremy Opriasa <opriasajbusiness@gmail.com>
2023-05-20 21:22:56 +0800

    feat:login model code is setup

---
+++ model/login.class.php
@@ -0,0 +1,34 @@
+<?php
+
+class login{
+  public $login;
+
+  public function __construct()
+  {
+    include(APPROOT."/config/config.php");
+    $this->login = new PDO(DSN, DBUSER, DBPASS);
+
+    if($this->login->errorCode()){
+      die("Something went wrong connecting to local database.");
+    } else{
+      echo "Connected to the local database";
+    }
+  }
+
+  public function loginAccount($username, $password, $c_password){
+    try{
+      $query = "SELECT * FROM admin WHERE username = $username AND password = $password AND c_password = $c_password";
+      $statement = $this->login->prepare($query);
+      $statement->execute();
+      $dataLogin = $statement->fetch(PDO::FETCH_ASSOC);
+      return $dataLogin;
+    } catch(PDOException $e){
+      throw $e;
+    }
+  }
+
+}
+
+
+
+?>
